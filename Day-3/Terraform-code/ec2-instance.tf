9da19db6f600c8e7ff22aac2ed6c925194159467
Balakrishna_cherukuri <bcherukuri@ehe.health>
2025-06-30 17:58:59 +0530

    Day3 code updated after class

--- Day-3/Terraform-code/ec2-instance.tf
+++ Day-3/Terraform-code/ec2-instance.tf
@@ -1,11 +1,18 @@
 resource "aws_instance" "web" {
+  count           = 3
   ami             = data.aws_ami.amzlinux2.id
-  instance_type   = "t3.micro"
-  key_name        = "terraform-training-key"
+  instance_type   = var.instance_type_map["dev"]
+  key_name        = var.key_name
   vpc_security_group_ids  = [aws_security_group.ssh_sg.id,aws_security_group.http_sg.id]
   user_data       = file("${path.module}/app1.sh")
 
   tags = {
-    Name = "webserver"
+    Name = "webserver-${count.index + 1}"
   }
 }
+
+
+
+
+
+
