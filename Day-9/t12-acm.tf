9cd40f629258eef7bcaf0cf0389d9e0355af8c4f
Balakrishna_cherukuri <bcherukuri@ehe.health>
2025-07-10 23:37:33 +0530

    added certificate related terraform code

---
+++ Day-9/t12-acm.tf
@@ -0,0 +1,17 @@
+data "aws_route53_zone" "dns_zone" {
+  name         = "balatraining.in"
+  private_zone = false
+}
+
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "6.0.0"
+  domain_name = data.aws_route53_zone.dns_zone.name
+  zone_id = data.aws_route53_zone.dns_zone.id
+  subject_alternative_names = [
+    "balatraining.in",
+    "*.balatraining.in"
+  ]
+  validation_method = "DNS"
+  tags = local.common_tags
+}
